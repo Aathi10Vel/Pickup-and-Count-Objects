ba306550bfbf374c882e58ab420c1872c1942b69
Aathi <37983032+Aathi10Vel@users.noreply.github.com>
2018-10-10 19:57:14 +0530

    Add files via upload

---
+++ CubeGroup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGroup : MonoBehaviour {
+
+    public GameObject[] cubeItems;
+    public int cubeCount = 0;
+    public int cubeAdded = 0;
+
+    GUISkin cubeMessage;
+
+
+	// Use this for initialization
+	void Start () {
+        cubeItems = GameObject.FindGameObjectsWithTag("CubeItem");
+        cubeCount = cubeItems.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cubeItems = GameObject.FindGameObjectsWithTag("CubeItem");
+        CubeCollected();
+	}
+
+    public void CubeCollected()
+    {
+        if (cubeItems.Length < cubeCount)
+        {
+            cubeAdded += 1;
+            cubeCount = cubeItems.Length;
+        }
+    }
+
+    public void OnGUI()
+    {
+        GUI.skin = cubeMessage;
+        GUI.Label(new Rect(0, 0, 120, 60), "Cube Added : " + cubeAdded);
+    }
+}
---
+++ PickUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject collectObject;
+    public bool takeObject;
+    GUISkin guiMessage;
+
+    private void OnTriggerEnter(Collider playerStatus)
+    {
+        if (playerStatus.gameObject.tag == "Player")
+        {
+            takeObject = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider playerStatus)
+    {
+        if (playerStatus.gameObject.tag == "Player")
+        {
+            takeObject = false;
+        }   
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (takeObject)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Destroy(collectObject.gameObject);
+            }
+        }
+	}
+
+    private void OnGUI()
+    {
+        GUI.skin = guiMessage;
+
+        if (takeObject)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 55, 120, 50), "PickUp Object");
+        }
+    }
+}
